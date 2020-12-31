4f9892c6df67b325b4bf4f2116871c00f5a6f147
Lisa <34863909+tottoma@users.noreply.github.com>
2021-01-01 02:34:34 +0900

    Update settings.py

--- myecs/settings.py
+++ myecs/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'note.apps.NoteConfig', # 追加
+    ##'note.apps.NoteConfig', # 追加
     'rest_framework',  #追加
     'django_filters',  #追加
     'import_export', #追加
