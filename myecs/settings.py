273c9db6d4b1d7a1b8cb549e176fbc943f044148
Lisa <34863909+tottoma@users.noreply.github.com>
2021-01-25 23:26:10 +0900

    Update settings.py

--- myecs/settings.py
+++ myecs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ffe#h4i5p2z*xew9g3x=*8hcjox5ihtqudqc3%-h_mthzia&&2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.11.6', '172.20.10.3', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.2.108', '172.20.10.3', '192.168.2.120', '.pythonanywhere.com']
 # G: 192.168.11.6 iphone: 172.20.10.3
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ##'note.apps.NoteConfig', # 追加
+    ##'note.apps.NoteConfig', # 追加 ############# pythonanywhereデプロイ時は戻す
     'rest_framework',  #追加
     'django_filters',  #追加
     'import_export', #追加
@@ -81,16 +81,13 @@ WSGI_APPLICATION = 'myecs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # 変更
-        'NAME': 'mynote$mysql_ecs', # プロジェクトで使用するデータベース名
-        'USER': 'mynote', # パソコンにインストールしたMySQLのユーザー名
-        'PASSWORD': 'root2020', # 同上。そのパスワード
-        'HOST': 'mynote.mysql.pythonanywhere-services.com',# いらない
+        'NAME': 'mysql_ecs', # プロジェクトで使用するデータベース名
+        'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': 'root', # 同上。そのパスワード
+        #'HOST': '127.0.0.1',いらない
         #'PORT': '3306',いらない
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-     'TEST': {
-          'NAME': 'mynote$test_mysql_ecs',
-     },
+    }
 }
 
 
@@ -131,22 +128,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-##STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(BASE_DIR) #<--
 STATICFILES_ROOT = os.path.join(PROJECT_ROOT, 'static')# <--
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')#うわあああああ
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-#)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')#うわあああああ
+##STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 ### 以下、追加 ###
 
 # rest_framework 設定
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ],
+    )
 }
 
 # 管理画面のログインを通常ログインとして流用する
