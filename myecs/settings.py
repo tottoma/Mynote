9390934b5920a2202507db7cd76f299e629bdfdd
Lisa <34863909+tottoma@users.noreply.github.com>
2021-01-26 00:44:13 +0900

    Update settings.py

--- myecs/settings.py
+++ myecs/settings.py
@@ -137,9 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')#うわあああああ
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')#うわあああああ
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
 ### 以下、追加 ###
 
 # rest_framework 設定
